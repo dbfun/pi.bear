d656af1eee19027af463644724c3de867aafee42
Totalpusher <totalpusher@gmail.com>
2017-10-15 15:04:46 +0200

    Добавлено мигание грибами

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var gpio = require("gpio"),
     express = require('express'),
     app = express(),
     isSwing = false,
+    isBlink = false,
     config = require('./etc/config');
 
 var toyBear = gpio.export(config.toyBear.IOPort, {
@@ -11,6 +12,22 @@ var toyBear = gpio.export(config.toyBear.IOPort, {
    ready: function() {
      // перестаем скакать
      toyBear.set(0);
+   }
+});
+
+var toyMushroomLeft = gpio.export(config.toyMushrooms.leftIOPort, {
+   direction: 'out',
+   ready: function() {
+     // перестаем скакать
+     toyMushroomLeft.set(0);
+   }
+});
+
+var toyMushroomRight = gpio.export(config.toyMushrooms.rightIOPort, {
+   direction: 'out',
+   ready: function() {
+     // перестаем скакать
+     toyMushroomRight.set(0);
    }
 });
 
@@ -26,8 +43,30 @@ function swingBear() {
   }, config.toyBear.swingDuration);
 }
 
+// Мигание грибами
+function blinkMushrooms() {
+  if(isBlink) return;
+  isBlink = true;
+  var status = 1;
+
+
+  var timer = setInterval(function(){
+    status = 1 - status;
+    toyMushroomLeft.set(status);
+    toyMushroomRight.set(1 - status);
+  }, 250);
+
+  setTimeout(function(){
+    clearInterval(timer);
+    toyMushroomLeft.set(0);
+    toyMushroomRight.set(0);
+    isBlink = false;
+  }, config.toyMushrooms.blinkDuration);
+}
+
 app.get('/bear/swing/', function(req, res) {
   swingBear();
+  blinkMushrooms();
   res.send('Lets swing bear!');
 });
 
--- telegram.js
+++ telegram.js
@@ -3,6 +3,7 @@
 var gpio = require("gpio"),
     TelegramBot = require('node-telegram-bot-api'),
     isSwing = false,
+    isBlink = false,
     config = require('./etc/config');
 
 var toyBear = gpio.export(config.toyBear.IOPort, {
@@ -10,6 +11,22 @@ var toyBear = gpio.export(config.toyBear.IOPort, {
    ready: function() {
      // перестаем скакать
      toyBear.set(0);
+   }
+});
+
+var toyMushroomLeft = gpio.export(config.toyMushrooms.leftIOPort, {
+   direction: 'out',
+   ready: function() {
+     // перестаем скакать
+     toyMushroomLeft.set(0);
+   }
+});
+
+var toyMushroomRight = gpio.export(config.toyMushrooms.rightIOPort, {
+   direction: 'out',
+   ready: function() {
+     // перестаем скакать
+     toyMushroomRight.set(0);
    }
 });
 
@@ -25,6 +42,27 @@ function swingBear() {
   }, config.toyBear.swingDuration);
 }
 
+// Мигание грибами
+function blinkMushrooms() {
+  if(isBlink) return;
+  isBlink = true;
+  var status = 1;
+
+
+  var timer = setInterval(function(){
+    status = 1 - status;
+    toyMushroomLeft.set(status);
+    toyMushroomRight.set(1 - status);
+  }, 250);
+
+  setTimeout(function(){
+    clearInterval(timer);
+    toyMushroomLeft.set(0);
+    toyMushroomRight.set(0);
+    isBlink = false;
+  }, config.toyMushrooms.blinkDuration);
+}
+
 function app() {
   var bot = new TelegramBot(config.telegram.token, { polling: true });
 
@@ -32,7 +70,8 @@ function app() {
     var chatId = msg.chat.id;
     bot.sendMessage(chatId, "Список команд:\n" +
       "/start - эта справочная информация\n" +
-      "/swing - начать раскачивать медведя"
+      "/swing - начать раскачивать медведя\n" +
+      "/blink - начать мигать грибами"
     );
   });
 
@@ -40,6 +79,12 @@ function app() {
     var chatId = msg.chat.id;
     swingBear();
     bot.sendMessage(chatId, 'Lets swing bear!');
+  });
+
+  bot.onText(/\/blink/, function (msg) {
+    var chatId = msg.chat.id;
+    blinkMushrooms();
+    bot.sendMessage(chatId, 'Lets blink mushrooms!');
   });
 
 }
